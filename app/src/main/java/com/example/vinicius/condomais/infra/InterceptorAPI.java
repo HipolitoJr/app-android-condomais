6c696de2b114f771d75c68f8346c7e63ddc56f74
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-05-30 17:29:26 -0300

    Configuração da API (interceptador) adicionado.

--- app/src/main/java/com/example/vinicius/condomais/infra/InterceptorAPI.java
+++ app/src/main/java/com/example/vinicius/condomais/infra/InterceptorAPI.java
@@ -1,8 +1,27 @@
 package com.example.vinicius.condomais.infra;
 
-public class InterceptorAPI {
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class InterceptorAPI implements Interceptor{
+
+    public static final String AUTHORIZATION = "Authorization";
+    public final String TOKEN;
 
     public InterceptorAPI(String token) {
+        this.TOKEN = token;
+    }
+
+    @Override
+    public Response intercept(Interceptor.Chain chain) throws IOException {
+        Request request = chain.request().newBuilder()
+                .addHeader(AUTHORIZATION, TOKEN)
+                .build();
+
+        return chain.proceed(request);
     }
 
 }
