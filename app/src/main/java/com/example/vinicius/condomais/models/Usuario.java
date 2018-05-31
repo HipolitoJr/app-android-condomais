2ee1419e8b69f797e332c230b6848ba247f1cb49
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-05-31 19:41:53 -0300

    Adicionados devidos atributos ao usuário.

--- app/src/main/java/com/example/vinicius/condomais/models/Usuario.java
+++ app/src/main/java/com/example/vinicius/condomais/models/Usuario.java
@@ -5,11 +5,16 @@ import com.google.gson.annotations.SerializedName;
 public class Usuario {
 
     @SerializedName("username") private String nomeDeUsuario;
+    @SerializedName("email") private String email;
     @SerializedName("password") private String senha;
 
-    public Usuario(String nomeDeUsuario, String senha) {
+    public Usuario(String nomeDeUsuario, String email, String senha) {
         this.nomeDeUsuario = nomeDeUsuario;
+        this.email = email;
         this.senha = senha;
     }
 
+    public String getNomeDeUsuario() {
+        return nomeDeUsuario;
+    }
 }
